89f6c03559594f4c7c5acb2018a96ec4b731eee3
Natalia <nataxawolf@gmail.com>
2023-12-06 21:16:10 +0300

    sem2

--- seminar_2/test_sem2.py
+++ seminar_2/test_sem2.py
@@ -1,5 +1,6 @@
 import yaml
-from module import Site
+import time
+import random, string
 
 with open('testdata.yaml') as f:
     testdata = yaml.safe_load(f)
@@ -19,6 +20,7 @@ def test_step1(site, login_xpath, pswd_xpath, btn_xpath, result_xpath):
     err_label = site.find_element('xpath', result_xpath)
     assert err_label.text == '401'
 
+
 # css_selector = 'span.mdc-text-field__ripple'
 # # указываем режим, сам слектор, и какое свойство из этого взять
 # print(site.get_element_property('css', css_selector, 'height'))
@@ -37,4 +39,28 @@ def test_step2(site, login_xpath, pswd_xpath, btn_xpath, result_success):
     assert login.text == 'Blog'
 
 
-
+# ДЗ№2
+def test_step3(site, login_xpath, pswd_xpath, btn_xpath, btn_create_post,
+               btn_save_post, tittle_post_xpath, description_post_xpath,
+               content_post_xpath, tittle_save_post):
+    input1 = site.find_element('xpath', login_xpath)
+    input1.send_keys(testdata['login'])
+    input2 = site.find_element('xpath', pswd_xpath)
+    input2.send_keys(testdata['password'])
+    btn = site.find_element('xpath', btn_xpath)
+    btn.click()
+    time.sleep(testdata['sleep_time'])
+    btn_create = site.find_element('xpath', btn_create_post)
+    btn_create.click()
+    time.sleep(testdata['sleep_time'])
+    input1 = site.find_element('xpath', tittle_post_xpath)
+    input1.send_keys('I want drink!')
+    input2 = site.find_element('xpath', description_post_xpath)
+    input2.send_keys('Vodka')
+    input2 = site.find_element('xpath', content_post_xpath)
+    input2.send_keys("".join(random.choices(string.ascii_lowercase + string.digits, k=300)))
+    btn_post = site.find_element('xpath', btn_save_post)
+    btn_post.click()
+    time.sleep(testdata['sleep_time'])
+    result = site.find_element('xpath', tittle_save_post)
+    assert result.text == 'I want drink!'
